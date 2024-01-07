de82608c72b4200b25d73366272ccd6b87e67cd1
Mai Tri Thanh <56778099+maitrithanh@users.noreply.github.com>
2024-01-07 19:00:13 +0700

    fix Can't resolve 'react-icons/io'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import { getCookie } from "cookies-next";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +15,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const token = getCookie("token", { cookies }) || null;
-  if (!token) {
-    redirect("/login");
-  }
   return (
     <html lang="en">
       <body className={inter.className}>
