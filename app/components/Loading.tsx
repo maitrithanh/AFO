28c1731f5cba3dab327bc59e309901af5f47ae36
Mai Tri Thanh <56778099+maitrithanh@users.noreply.github.com>
2024-01-10 15:26:12 +0700

    change color loading

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
       <div className="h-screen flex items-center justify-center" role="status">
         <svg
           aria-hidden="true"
-          className="w-14 h-14 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="w-14 h-14 text-gray-200 animate-spin dark:text-gray-600 fill-[#F8853E]"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
