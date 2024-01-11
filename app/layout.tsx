d4622a4bd3fb83d5674155f629b2f6b99dadf169
Mai Tri Thanh <56778099+maitrithanh@users.noreply.github.com>
2024-01-11 16:32:47 +0700

    fix Mixed Content API

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={inter.className}>
         <div className="relative">
           <Toaster />
