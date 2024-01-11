e3572035908c1d988ce1de4c173ee90ead7c43d0
H-a-n-k <haunguyen231122@gmail.com>
2024-01-12 00:40:45 +0700

    Update page.tsx

--- app/student/profile/page.tsx
+++ app/student/profile/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const ProfilePage = () => {
   return (
     <div>
-      {/* <ProfileCard /> */}
-      <ProfilePage />
+      <ProfileCard />
+      {/* <ProfilePage /> */}
     </div>
   );
 };
