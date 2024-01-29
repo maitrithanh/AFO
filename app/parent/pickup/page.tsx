1cfb55b48cde411e8ea320b95b602f81dcd710f2
Mai Tri Thanh <56778099+maitrithanh@users.noreply.github.com>
2024-01-29 16:16:22 +0700

    fix ui

--- app/parent/pickup/page.tsx
+++ app/parent/pickup/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import DefaultImage from "@/app/components/defaultImage";
 import PickUpListRes from "@/types/PickUpListRes";
@@ -11,116 +11,161 @@ import PutPickupDialog from "./putDialog";
 import { callApiWithToken } from "@/utils/callApi";
 import toast from "react-hot-toast";
 
-const PickUpPage = () => { 
-    const [openDialog, SetOpenDialog] = useState("");
-    const [refresh, setRefresh] = useState(false);
-    const [currData, setCurrData] = useState<PickUpListRes | null>(null) // data of selected pickup
+const PickUpPage = () => {
+  const [openDialog, SetOpenDialog] = useState("");
+  const [refresh, setRefresh] = useState(false);
+  const [currData, setCurrData] = useState<PickUpListRes | null>(null); // data of selected pickup
 
-    const { data: pickups } = useFetch<PickUpListRes[]>('parent/pickuplist', null, refresh);
+  const { data: pickups } = useFetch<PickUpListRes[]>(
+    "parent/pickuplist",
+    null,
+    refresh
+  );
 
-    const OnEdit = (x: PickUpListRes) => { 
-        setCurrData(x)
-        SetOpenDialog('edt')
-    }
+  const OnEdit = (x: PickUpListRes) => {
+    setCurrData(x);
+    SetOpenDialog("edt");
+  };
 
-    const OnAdd = () => {
-        SetOpenDialog('add')
-    }
+  const OnAdd = () => {
+    SetOpenDialog("add");
+  };
 
-    const OnDelete = (x: PickUpListRes) => {
-        if (!confirm(`Xác nhận xóa ${x.fullName} khỏi danh sách đón hộ?`)) return;
-        callApiWithToken()
-            .delete('parent/deletepickup/' + x.id)
-            .then(() => {
-                toast.success('Đã xóa');
-                setRefresh(x => !x);
-            }).catch(() => {
-                toast.error('Có lỗi xảy ra');
-            })
-    }
+  const OnDelete = (x: PickUpListRes) => {
+    if (!confirm(`Xác nhận xóa ${x.fullName} khỏi danh sách đón hộ?`)) return;
+    callApiWithToken()
+      .delete("parent/deletepickup/" + x.id)
+      .then(() => {
+        toast.success("Đã xóa");
+        setRefresh((x) => !x);
+      })
+      .catch(() => {
+        toast.error("Có lỗi xảy ra");
+      });
+  };
 
-    const onToggleStatus = (id: number) => { 
-        callApiWithToken()
-            .get('parent/togglepickup/' + id)
-            .then(() => {
-                setRefresh(x => !x);
-            }).catch(() => {
-                toast.error('Có lỗi xảy ra');
-            })
-    }
+  const onToggleStatus = (id: number) => {
+    callApiWithToken()
+      .get("parent/togglepickup/" + id)
+      .then(() => {
+        setRefresh((x) => !x);
+      })
+      .catch(() => {
+        toast.error("Có lỗi xảy ra");
+      });
+  };
 
-    //TODO:
-    //Dialog edit 
-    //toggle status
-    //delete button
+  //TODO:
+  //Dialog edit
+  //toggle status
+  //delete button
 
-    return <div className="bg-white w-2/3 m-auto px-10 pt-10 rounded-xl">
-        <h2 className="text-2xl font-bold">Danh sách người đón hộ</h2>
-        <p>Những người được cấp quyền đưa đón trẻ thay cho người giám hộ</p>
+  return (
+    <div className="bg-white md:w-2/3 m-auto px-10 pt-10 rounded-xl">
+      <h2 className="text-2xl font-bold">Danh sách người đón hộ</h2>
+      <p>Những người được cấp quyền đưa đón trẻ thay cho người giám hộ</p>
 
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded my-5" onClick={OnAdd}>
-            + Thêm mới
-        </button>
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded my-5"
+        onClick={OnAdd}
+      >
+        + Thêm mới
+      </button>
 
-        <ul role="list" className="divide-y divide-gray">
-            {
-                pickups && pickups.map(x => <>
-                    <li className="flex gap-x-6 py-5 group" key={x.id}>
-                        <div className="w-5/12 flex min-w-0 gap-x-4">
-                            <DefaultImage
-                                key={x?.avatar}
-                                img={getImageUrl(x?.avatar)}
-                                fallback="/avatar.webp"
-                                className={`h-24 w-24 flex-none rounded-full bg-gray-50`}
-                            />
-                            {/* <img className="h-24 w-24 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" /> */}
-                            <div className="min-w-0 flex-auto flex flex-col justify-evenly">
-                                <p className="text-xl font-semibold leading-6 text-gray-900">{x.fullName}</p>
-                                <p className="mt-1 truncate text-l leading-5 text-gray-500">sđt: {x.phoneNumber}</p>
-                            </div>
+      <ul role="list" className="divide-y divide-gray">
+        {pickups &&
+          pickups.map((x) => (
+            <>
+              <li className="flex md:gap-x-6 py-5 group w-full" key={x.id}>
+                <div className="md:w-5/12 flex min-w-0 gap-x-4">
+                  <DefaultImage
+                    key={x?.avatar}
+                    img={getImageUrl(x?.avatar)}
+                    fallback="/avatar.webp"
+                    className={`h-24 w-24 flex-none rounded-full bg-gray-50`}
+                    custom="w-[100px] h-[100px]"
+                  />
+                  {/* <img className="h-24 w-24 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" /> */}
+                  <div className="min-w-0 flex-auto flex flex-col justify-evenly">
+                    <p className="text-xl font-semibold leading-6 text-gray-900">
+                      {x.fullName}
+                    </p>
+                    <p className="mt-1 truncate text-l leading-5 text-gray-500">
+                      SĐT: {x.phoneNumber}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="md:w-6/12 hidden shrink-0 sm:flex sm:flex-col sm:items-end justify-evenly">
+                  <p className="text-xl leading-6 text-gray-900">
+                    Địa chỉ: {x.address}
+                  </p>
+                  <div className="mt-1 flex items-center gap-x-1.5">
+                    {x.status ? (
+                      <>
+                        <div
+                          className="cursor-pointer flex-none rounded-full bg-emerald-500/20 p-1"
+                          title="Hủy bỏ"
+                          onClick={() => onToggleStatus(x.id)}
+                        >
+                          <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
                         </div>
-
-                        <div className="w-5/12 hidden shrink-0 sm:flex sm:flex-col sm:items-end justify-evenly">
-                            <p className="text-xl leading-6 text-gray-900">đ/c: {x.address}</p>
-                            <div className="mt-1 flex items-center gap-x-1.5">
-                                {
-                                    x.status ?
-                                    <>
-                                        <div className="cursor-pointer flex-none rounded-full bg-emerald-500/20 p-1" title="Hủy bỏ" onClick={() => onToggleStatus(x.id)}>
-                                            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
-                                        </div>
-                                        <p className="text-l leading-5 text-gray-500">Được đón hộ</p>       
-                                    </>
-                                    :
-                                    <>
-                                        <div className="cursor-pointer flex-none rounded-full bg-red-500/20 p-1" title="Cho phép" onClick={() => onToggleStatus(x.id)}>
-                                            <div className="h-1.5 w-1.5 rounded-full bg-red-500"></div>
-                                        </div>
-                                            <p className="text-l leading-5 text-gray-500"><del>Được đón hộ</del></p>
-                                    </>
-                                }
-                            </div>
+                        <p className="text-l leading-5 text-gray-500">
+                          Được đón hộ
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        <div
+                          className="cursor-pointer flex-none rounded-full bg-red-500/20 p-1"
+                          title="Cho phép"
+                          onClick={() => onToggleStatus(x.id)}
+                        >
+                          <div className="h-1.5 w-1.5 rounded-full bg-red-500"></div>
                         </div>
+                        <p className="text-l leading-5 text-gray-500">
+                          <del>Được đón hộ</del>
+                        </p>
+                      </>
+                    )}
+                  </div>
+                </div>
 
-                        <div className="invisible group-hover:visible flex flex-col justify-evenly">
-                            <div className="rounded-full p-2 text-blue-600 border-blue-600 border-solid border-2 cursor-pointer" onClick={() => OnEdit(x)}>
-                                <FaPen />
-                            </div>
+                <div className="invisible group-hover:visible flex flex-col justify-evenly">
+                  <div
+                    className="rounded-full p-2 text-blue-600 border-blue-600 border-solid border-2 cursor-pointer"
+                    onClick={() => OnEdit(x)}
+                  >
+                    <FaPen />
+                  </div>
 
-                            <div className="rounded-full p-2 text-red-600 border-red-600 border-solid border-2 cursor-pointer" onClick={() => OnDelete(x)}>
-                                <FaDeleteLeft />
-                            </div>
-                        </div>
-                    </li>
-                </>)
-            }
-            
-        </ul>
+                  <div
+                    className="rounded-full p-2 text-red-600 border-red-600 border-solid border-2 cursor-pointer"
+                    onClick={() => OnDelete(x)}
+                  >
+                    <FaDeleteLeft />
+                  </div>
+                </div>
+              </li>
+            </>
+          ))}
+      </ul>
 
-        {/* put dialog */}
-        {openDialog && <PutPickupDialog onClose={() => { SetOpenDialog("") }} onSuccess={() => { setRefresh(x => !x) }} mode={openDialog} defaultData={currData} /> }
+      {/* put dialog */}
+      {openDialog && (
+        <PutPickupDialog
+          onClose={() => {
+            SetOpenDialog("");
+          }}
+          onSuccess={() => {
+            setRefresh((x) => !x);
+          }}
+          mode={openDialog}
+          defaultData={currData}
+        />
+      )}
     </div>
-}
+  );
+};
 
-export default PickUpPage
-
+export default PickUpPage;
--- app/parent/pickup/putDialog.tsx
+++ app/parent/pickup/putDialog.tsx
@@ -11,208 +11,221 @@ import { toYMD } from "@/utils/dateTime";
 import { getImageUrl } from "@/utils/image";
 
 interface Prop {
-    onClose: () => void
-    onSuccess: () => void
-    mode: string,
-    defaultData: PickUpListRes | null
+  onClose: () => void;
+  onSuccess: () => void;
+  mode: string;
+  defaultData: PickUpListRes | null;
 }
 
 const PutPickupDialog = ({ onSuccess, onClose, mode, defaultData }: Prop) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [currAvatar, setCurrAvatar] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currAvatar, setCurrAvatar] = useState<File | null>(null);
 
-    if (defaultData) {
-        defaultData.birthDay = toYMD(defaultData?.birthDay);
+  if (defaultData) {
+    defaultData.birthDay = toYMD(defaultData?.birthDay);
+  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues:
+      defaultData && mode == "edt"
+        ? defaultData
+        : {
+            fullName: "",
+            phoneNumber: "",
+            birthDay: "",
+            address: "",
+            note: "",
+            avatar: null,
+          },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    const formData = new FormData();
+    for (const key in data) {
+      formData.append(key, data[key]);
     }
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FieldValues>({
-        defaultValues: (defaultData && mode == 'edt') ? defaultData : {
-            fullName: '',
-            phoneNumber: '',
-            birthDay: '',
-            address: '',
-            note: '',
-            avatar: null
-        },
-    });
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true);
-        const formData = new FormData();
-        for (const key in data) {
-            formData.append(key, data[key]);
-        }
+    if (currAvatar) {
+      formData.append("avatar", currAvatar);
+    }
 
-        if (currAvatar) {
-            formData.append("avatar", currAvatar);
-        }
+    if (mode === "add") {
+      callApiWithToken()
+        .post("parent/addpickup", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data", // Specify content type
+          },
+        })
+        .then((res) => {
+          toast.success("Đã cập nhật");
+          onClose();
+          onSuccess();
+        })
+        .catch((err) => {
+          toast.error(err?.response?.data?.error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else if (mode === "edt") {
+      callApiWithToken()
+        .put("parent/editpickup/" + defaultData?.id, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data", // Specify content type
+          },
+        })
+        .then((res) => {
+          toast.success("Đã cập nhật");
+          onClose();
+          onSuccess();
+        })
+        .catch((err) => {
+          toast.error(err?.response?.data?.error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  };
 
-        if (mode === 'add') {
-            callApiWithToken()
-                .post("parent/addpickup", formData, {
-                    headers: {
-                        "Content-Type": "multipart/form-data", // Specify content type
-                    },
-                })
-                .then((res) => {
-                    toast.success("Đã cập nhật");
-                    onClose();
-                    onSuccess();
-                })
-                .catch((err) => {
-                    toast.error(err?.response?.data?.error);
-                })
-                .finally(() => {
-                    setIsLoading(false);
-                });
-        } else if (mode === 'edt') { 
-            callApiWithToken()
-                .put("parent/editpickup/" + defaultData?.id, formData, {
-                    headers: {
-                        "Content-Type": "multipart/form-data", // Specify content type
-                    },
-                })
-                .then((res) => {
-                    toast.success("Đã cập nhật");
-                    onClose();
-                    onSuccess();
-                })
-                .catch((err) => {
-                    toast.error(err?.response?.data?.error);
-                })
-                .finally(() => {
-                    setIsLoading(false);
-                });
-        }
-    };
+  const onClickOut = (e: any) => {
+    if (e.target != e.currentTarget) return;
+    onClose();
+  };
 
-    const onClickOut = (e: any) => {
-        if (e.target != e.currentTarget) return;
-        onClose();
-    };
+  return (
+    <div
+      onClick={onClickOut}
+      className="fixed z-50 top-0 left-0 w-full h-full flex items-center bg-black bg-opacity-40 justify-center"
+    >
+      <form
+        className="space-y-6 p-10 bg-white md:rounded-lg md:h-fit h-full w-[640px] mx-2 overflow-none"
+        action="#"
+        method="POST"
+      >
+        <h3 className="text-2xl text-justify font-bold">
+          {mode === "add"
+            ? "Thêm người đưa đón"
+            : mode == "edt"
+            ? "Sửa thông tin người đưa đón"
+            : "???"}
+        </h3>
 
-    return (
-        <div
-            onClick={onClickOut}
-            className="fixed z-50 top-0 left-0 w-full h-full flex items-center bg-black bg-opacity-40 justify-center"
-        >
-            <form
-                className="space-y-6 p-10 bg-white rounded-3xl w-[440px] mx-2 max-h-[80%] overflow-auto"
-                action="#"
-                method="POST"
-            >
-                <h3 className="text-2xl text-justify font-bold">
-                    {
-                        mode === 'add' ? 'Thêm người đưa đón'
-                            : 
-                        mode == 'edt' ? 'Sửa thông tin người đưa đón' : '???'
-                    }
-                </h3>
+        <div className="mt-2 border-slate-300 border-2 rounded-md p-4">
+          <div className="flex items-center mb-4">
+            <span className="mr-4 text-slate-500">Avatar: </span>
+            {!currAvatar && defaultData?.avatar && mode === "edt" && (
+              <img
+                src={getImageUrl(defaultData.avatar)}
+                alt=""
+                className="w-14 h-14 rounded-full"
+              />
+            )}
 
-                <div className="mt-2 border-slate-300 border-2 rounded-md p-4">
-                    <div className="flex items-center mb-4"> 
-                        <span className="mr-4 text-slate-500">Avatar: </span>
-                        {!currAvatar && defaultData?.avatar && mode === 'edt' &&
-                            <img src={getImageUrl(defaultData.avatar)} alt="" className="w-14 h-14 rounded-full" />
-                        }
+            {currAvatar && (
+              <img
+                src={URL.createObjectURL(currAvatar)}
+                alt="Current Avatar"
+                className="w-14 h-14 rounded-full"
+              />
+            )}
+          </div>
 
-                        {currAvatar && (
-                            <img src={URL.createObjectURL(currAvatar)} alt="Current Avatar"
-                                className="w-14 h-14 rounded-full"/>
-                        )}
-                    </div>
-
-                    <input
-                        id={'avatar'}
-                        {...register('avatar', { required: mode === 'add' })} 
-                        type={'file'}
-                        onChange={(e) => setCurrAvatar(e.target.files![0])}
-                    />
-                </div>
-
-                <div>
-                    <div className="mt-2">
-                        <Input
-                            id="fullName"
-                            label="Họ tên"
-                            type={'text'}
-                            disabled={isLoading}
-                            register={register}
-                            errors={errors}
-                            required
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="mt-2">
-                        <Input
-                            id="phoneNumber"
-                            label="Số điện thoại"
-                            type={'text'}
-                            disabled={isLoading}
-                            register={register}
-                            errors={errors}
-                            required
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="mt-2">
-                        <Input
-                            id="birthDay"
-                            label="Ngày sinh"
-                            type={'date'}
-                            disabled={isLoading}
-                            register={register}
-                            errors={errors}
-                            required
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="mt-2">
-                        <Input
-                            id="address"
-                            label="Địa chỉ"
-                            type={'text'}
-                            disabled={isLoading}
-                            register={register}
-                            errors={errors}
-                            required
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div className="mt-2">
-                        <Input
-                            id="note"
-                            label="Chú thích"
-                            type={'text'}
-                            disabled={isLoading}
-                            register={register}
-                            errors={errors}
-                            required
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <Button
-                        loading={isLoading}
-                        label={"Xác nhận"}
-                        onClick={handleSubmit(onSubmit)}
-                    />
-                </div>
-            </form>
+          <input
+            id={"avatar"}
+            {...register("avatar", { required: mode === "add" })}
+            type={"file"}
+            onChange={(e) => setCurrAvatar(e.target.files![0])}
+          />
         </div>
-    );
+
+        <div>
+          <div className="mt-2">
+            <Input
+              id="fullName"
+              label="Họ tên"
+              type={"text"}
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="mt-2">
+            <Input
+              id="phoneNumber"
+              label="Số điện thoại"
+              type={"text"}
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="mt-2">
+            <Input
+              id="birthDay"
+              label="Ngày sinh"
+              type={"date"}
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="mt-2">
+            <Input
+              id="address"
+              label="Địa chỉ"
+              type={"text"}
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="mt-2">
+            <Input
+              id="note"
+              label="Chú thích"
+              type={"text"}
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <Button
+            loading={isLoading}
+            label={"Xác nhận"}
+            onClick={handleSubmit(onSubmit)}
+          />
+        </div>
+        <div className="block md:hidden">
+          <Button loading={isLoading} label={"Thoát"} onClick={onClose} />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PutPickupDialog;
