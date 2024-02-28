d39e703ef57e58a80d2a236e905051b9b91e5679
H-a-n-k <haunguyen231122@gmail.com>
2024-02-28 16:46:28 +0700

    table template

--- app/admin/children/add/page.tsx
+++ app/admin/children/add/page.tsx
@@ -99,7 +99,7 @@ const AddChildrenPage = () => {
 
   return (
     <div className="w-[60%] mx-auto">
-      <h2 className="font-bold text-xl mb-3">Thông tin trẻ: </h2>
+      <h2 className="font-bold text-xl mb-3">Thông tin trẻ</h2>
       <div>
         <ChildrenPutForm
           data={dataChildren}
@@ -109,25 +109,29 @@ const AddChildrenPage = () => {
       </div>
 
       <div className="flex justify-between mt-[20px]">
-        <h2 className="font-bold text-xl mb-3">Thông tin người giám hộ: </h2>
-        <label className="inline-flex items-center mb-5 cursor-pointer">
-          <input
-            type="checkbox"
-            value=""
-            className="sr-only peer"
-            checked={registerdParent}
-            onChange={() => {
-              setRegisterdParent((x) => !x);
-            }}
-          />
-          <span className="text-sm font-medium  peer-checked:text-gray-500 text-green-600">
-            NGH mới
-          </span>
-          <div className="relative w-11 h-6 bg-green-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 mx-3"></div>
-          <span className="text-sm font-medium text-gray-500 peer-checked:text-blue-600">
-            NGH đã đăng ký
-          </span>
-        </label>
+        <h2 className="font-bold text-xl mb-3">Thông tin người giám hộ</h2>
+        
+        <div>
+          <div className="font-bold mb-2">Trạng thái người giám hộ</div>
+          <label className="inline-flex items-center mb-5 cursor-pointer">
+            <input
+              type="checkbox"
+              value=""
+              className="sr-only peer"
+              checked={registerdParent}
+              onChange={() => {
+                setRegisterdParent((x) => !x);
+              }}
+            />
+            <span className="text-sm font-medium  peer-checked:text-gray-500 text-green-600">
+              Mới
+            </span>
+            <div className="relative w-11 h-6 bg-green-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 mx-3"></div>
+            <span className="text-sm font-medium text-gray-500 peer-checked:text-blue-600">
+              Đã đăng ký
+            </span>
+          </label>
+        </div>
       </div>
       {registerdParent ? (
         <div>
---
+++ app/admin/testTable/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import TableTemplate, { TableTemplateAction, TableTemplateColumn, TableTemplateSort } from "@/app/components/shared/TableTemplate";
+import ParentListRes from "@/types/ParentListRes";
+import { compareName } from "@/utils/compare";
+import useFetch from "@/utils/useFetch";
+import { link } from "fs";
+import { useMemo } from "react";
+
+const Columns: TableTemplateColumn<ParentListRes>[] = [
+    {
+        title: "Tên Phụ Huynh",
+        getData: (x) => x.fullName
+    },
+    {
+        title: "Số điện thoại",
+        getData: (x) => x.phoneNumber
+    },
+    {
+        title: "Giới tính",
+        getData: (x) => x.gender ? "Nam" : "Nữ"
+    },
+    {
+        title: "Ngày sinh",
+        getData: (x) => x.birthDay
+    },
+    {
+        title: "Địa chỉ",
+        getData: (x) => x.address
+    },
+    {
+        title: "Tên Trẻ",
+        getData: (x) => <pre>
+            {x.children.map((x) => x.fullName).join(",\n")}
+        </pre>
+    },
+];
+
+const searchCols = [Columns[0], Columns[1],];
+
+const Action: TableTemplateAction<ParentListRes> = {
+    getLink: (x) => `/admin/listparent/${x.id}`
+}
+
+const getJoinDate = (a: ParentListRes): string => {
+    return a.children.reduce((res, curr) =>
+        curr.joinDate > res.joinDate ? curr : res
+    ).joinDate;
+};
+
+const sorts: TableTemplateSort<ParentListRes>[] = [
+    {
+        title: 'Mới nhất',
+        compare: (a, b) => getJoinDate(a) <= getJoinDate(b) ? 1 : -1
+    },
+    {
+        title: 'Tên (A-Z)',
+        compare: (a, b) => compareName(a.fullName, b.fullName)
+    },
+    {
+        title: 'Tên (Z-A)',
+        compare: (a, b) => -compareName(a.fullName, b.fullName)
+    }
+]
+
+const TestTable = () => { 
+    const { data: dataParent } = useFetch<ParentListRes[]>("Parent/GetList");
+
+    return <TableTemplate<ParentListRes>
+        title="Danh sách phụ huynh"
+        dataSource={dataParent || []}
+        columns={Columns}
+        actions={[Action]}
+        searchColumns={searchCols}
+        searchPlaceHolder="Nhập tên hoặc sđt..."
+        sortOptions={sorts}
+    />
+}
+
+export default TestTable
