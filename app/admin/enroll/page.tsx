d7774e3a09a8314d24dfd173a04ffd14aaa95ba2
H-a-n-k <haunguyen231122@gmail.com>
2024-06-02 22:55:48 +0700

    Update page.tsx

--- app/admin/enroll/page.tsx
+++ app/admin/enroll/page.tsx
@@ -111,7 +111,7 @@ const EnrollPage = () => {
   };
 
   const handleUpdateStatusEnroll = () => {
-    updateStatusEnroll(selectedRequest?.idRequest ?? '');
+    updateStatusEnroll(selectedRequest?._id ?? '');
     sendConfirmationEmail(selectedRequest);
     setOnUpdate(true);
     handleRefresh();
